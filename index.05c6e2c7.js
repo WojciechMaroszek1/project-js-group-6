7fcd72ef69c8bd881cd92670da99955f902e062c
WojciechMaroszek1 <122974346+WojciechMaroszek1@users.noreply.github.com>
2023-09-30 08:16:50 +0000

    Deploying to gh-pages from @ WojciechMaroszek1/project-js-group-6@5d54ca206c3d96e0fe9ac0bbc578002c848f87cf 🚀

---
+++ index.05c6e2c7.js
@@ -0,0 +1,2 @@
+function openToggle(){document.getElementById("groupInfo").style.height="300px"}function closeToggle(){document.getElementById("groupInfo").style.height="0"}
+//# sourceMappingURL=index.05c6e2c7.js.map
