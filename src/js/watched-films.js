495cc304d56815ada1f1b93596af296c0285acb2
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2023-10-09 00:55:02 +0200

    LIBRARY WORKS FINALLY

--- src/js/watched-films.js
+++ src/js/watched-films.js
@@ -26,7 +26,6 @@ const btnQueue = document.getElementById('btn-queue');
 
 document.addEventListener('DOMContentLoaded', function () {
   btnWatched.addEventListener('click', () => {
-    // console.log('header_WATCHED');
     const watchedMoviesArray = JSON.parse(localStorage.getItem('watched'));
     if (watchedMoviesArray) {
       loadMovies(watchedMoviesArray);
@@ -55,26 +54,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Koniec obsługi przycisków
 
-  function addModalListenerFunction() {
-    document.addEventListener('DOMContentLoaded', function () {
-      //Event listener i funkcja otwierająca modal
-      movieList.addEventListener('click', event => {
-        if (event.target.closest('.movie-card')) {
-          document.body.classList.add('modal-film__stop-scrolling');
-          const modalId = event.target.closest('.movie-card').getAttribute('data-modal-target');
-          fetchMovies(event.target.closest('.movie-card').dataset.id)
-            .then(({ data, genreList }) => {
-              renderModal(data, genreList);
-            })
-            .catch(error => console.log(error));
-          const modal = document.getElementById('modal-film');
-          if (modal) {
-            modal.classList.remove('is-hidden');
-          }
+  document.addEventListener('DOMContentLoaded', function () {
+    //Event listener i funkcja otwierająca modal
+    libraryGallery.addEventListener('click', event => {
+      console.log('movieList');
+      if (event.target.closest('.card')) {
+        document.body.classList.add('modal-film__stop-scrolling');
+        const modalId = event.target.closest('.card').getAttribute('data-modal-target');
+        fetchMovies(event.target.closest('.card').dataset.id)
+          .then(({ data, genreList }) => {
+            renderModal(data, genreList);
+          })
+          .catch(error => console.log(error));
+        const modal = document.getElementById('modal-film');
+        if (modal) {
+          modal.classList.remove('is-hidden');
         }
-      });
+      }
     });
-  }
+  });
 
   async function loadMovies(movieIds) {
     try {
@@ -118,20 +116,22 @@ document.addEventListener('DOMContentLoaded', function () {
           poster_path != null ? `https://image.tmdb.org/t/p/w500${poster_path}` : noMoviePoster;
         return `
   
-            <li class="movie-card" data-id="${id}" data-type="movie">
-              <div class="movie-card__box">
-                <img class="movie-card__img" src="${moviePoster}" data-img="${moviePoster}" loading="lazy" alt="${movieTitle}" />
-              </div>
-              <h2 class="movie-card__heading">${movieTitle}</h2>
-  
-              <span class="movie-card__caption">  ${releaseDate} <span class="library-vote"> ${vote} </span></span>
+            <li class="card" id="toogle-film-card" data-id="${id}" data-type="movie" data-modal-open>
+             
+                <img class="card__img" src="${moviePoster}" data-img="${moviePoster}" loading="lazy" alt="${movieTitle}" />
+                             <div class="card__info">
+        <p class="card__title">${movieTitle}</p>
+        <p class="card__desc">${releaseDate}</p>
+      </div>
               </li>
+              
+
             `;
       })
       .join('');
 
     libraryGallery.innerHTML = markup;
-    addModalListenerFunction();
+    // addModalListenerFunction();
   }
 });
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
--- src/library.js
+++ src/library.js
@@ -1,4 +1,5 @@
 import { showLoader, hideLoader } from './loader';
 import './sass/main.scss';
 import './js/watched-films';
+// import './js/tmdb_api';
 import './js/dark_mode';
